eaa2ab8cd76b52a78e9ac9612c917ffccb99b0f0
Karthikeya <kamalkarthikeyakumar@gmail.com>
2026-01-31 09:53:31 +0530

    more backend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,25 @@
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/database.js";
+import app from "./app.js";
 
 connectDB();
+const startServer = async()=>{
+    try {
+    await connectDB();
+
+    app.on("error",(error)=>{
+        console.log("ERROR",error);
+        throw error;
+    });
+
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port :${process.env.PORT}`);
+
+    });
+    } catch (err) {
+        console.log("DB connection failed",err);
+    }
+}
+
+startServer();
